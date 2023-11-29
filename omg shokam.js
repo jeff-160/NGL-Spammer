50dee623d9bf61bdb567a47022786ad3a722e65a
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-11-29 22:59:02 +0800

    Update omg shokam.js

--- omg shokam.js
+++ omg shokam.js
@@ -2,6 +2,7 @@
 // @name         omg shokam
 // @namespace    http://tampermonkey.net/
 // @version      0.1
+// @icon         https://isphs.hci.edu.sg/data/photos/%7B21FFCAF7-04B4-4266-931D-17A5C82EEB8E%7D.jpg
 // @description  omg shokam
 // @author       WAP Industries
 // @match        https://ngl.link/junenlom1
