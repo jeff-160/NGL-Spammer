ecf692fb68555bd9eac8d7a8f0bac01882dbea40
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-11-29 17:33:23 +0800

    Add files via upload

--- omg shokam.py
+++ omg shokam.py
@@ -1,8 +1,20 @@
 from pyautogui import hotkey
 from webbrowser import open
 from time import sleep
+from random import randint
 
-for i in range(int(input("Enter number of messages: "))):
-    open("https://ngl.link/junenlom")
-    sleep(4)
-    hotkey('ctrl', 'w')
+batch_size = 10
+
+def main():
+    count = int(input("Number of messages: "))
+    while count>0:
+        batch = batch_size if count>=batch_size else count
+        for i in range(batch):
+            open("https://ngl.link/junenlom1")
+            count-=1
+        sleep(randint(3, 7))
+        for i in range(batch): 
+            hotkey("ctrl", "w")
+
+if __name__=="__main__":
+    main()
