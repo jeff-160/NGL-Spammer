6678598bb2341128b803dc90908c5ba46dcb0641
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-11-30 15:30:11 +0800

    Update omg shokam.js

--- omg shokam.js
+++ omg shokam.js
@@ -5,7 +5,7 @@
 // @icon         https://isphs.hci.edu.sg/data/photos/%7B21FFCAF7-04B4-4266-931D-17A5C82EEB8E%7D.jpg
 // @description  omg shokam
 // @author       WAP Industries
-// @match        https://ngl.link/junenlom1
+// @match        https://ngl.link/junenlom2/3words
 // @grant        none
 // ==/UserScript==
 
@@ -13,11 +13,8 @@
     'use strict';
 
     setTimeout(()=>{
-        const T = document.querySelector('textarea[placeholder="send me anonymous messages..."]'),
-            B = document.querySelector(".submit");
-
-        T.value = "omg shokam"
-        B.click()
+        document.querySelector(".dice-button").click()
+        document.querySelector(".submit").click()
     }, 500)
 
 })();
